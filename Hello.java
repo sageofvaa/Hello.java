1cf705e7c140077bf6d52e247b28c5feebe194e4
sageofvaa <54364531+sageofvaa@users.noreply.github.com>
2019-08-21 10:07:24 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,25 @@
+/*****************************************
+* Java Program Name
+******************************************
+* Project/Program description
+*-----------------------------------------
+* Student First Name Last Name
+* Date
+* Course Number and Section
+******************************************/
+
+public class Hello {
+	public static void printHeading() {
+	System.out.println("Julien Huff");
+	System.out.println("Lab 1");
+	System.out.println("Follow lab instructions");
+	System.out.println("Version date");
+	System.out.println("CMSC 255, Section 003");
+	System.out.println();
+	}
+
+	public static void main(String [] args) {
+	printHeading();
+	System.out.println("Hello");
+	}
+}
